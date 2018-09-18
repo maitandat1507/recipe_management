e37cf8eb1786a61d4cfb94c49b6ad8717aa5aadc
Mai Tấn Đạt <maitandat1507@gmail.com>
2018-09-18 11:28:13 +0700

    how import/export work

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,13 @@
-// Global app controller
-import num from './test';
-const x = 23;
+import str from './models/Search';
 
-console.log(`num = ${num}, imported from another test another! Variable x is ${x}`);
+// normal way
+// import {add, multiply, ID} from './views/searchView';
+
+// using alias command
+// import {add as a, multiply as m, ID} from './views/searchView';
+
+// import all
+import * as searchView from './views/searchView';
+
+// console.log(`Using imported functions! ${add(ID, 2)} and ${multiply(3,5)}. ${str}`);
+console.log(`Using imported functions! ${searchView.add(searchView.ID, 6)} and ${searchView.multiply(3,5)}. ${str}`);
