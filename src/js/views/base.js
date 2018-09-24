7f7411fc7f5f6feadc83012aa724003faf226dc4
Mai Tấn Đạt <maitandat1507@gmail.com>
2018-09-24 16:06:29 +0700

    clear spinning icon when data loaded

--- src/js/views/base.js
+++ src/js/views/base.js
@@ -5,13 +5,23 @@ export const elements = {
     searchResList: document.querySelector('.results__list'),
 };
 
+export const elementStrings = {
+    loader: 'loader'
+};
+
 export const renderLoader = parent => {
     const loader = `
-        <div class="loader">
+        <div class="${elementStrings.loader}">
             <svg>
-                <use xlink:href="img/icons.svg#icon-cw"></use>
+                <use href="img/icons.svg#icon-cw"></use>
             </svg>
         </div>
     `;
     parent.insertAdjacentHTML('afterbegin', loader);
 };
+
+export const clearLoader = () => {
+    const loader = document.querySelector(`.${elementStrings.loader}`);
+
+    if (loader) loader.parentElement.removeChild(loader);
+}
