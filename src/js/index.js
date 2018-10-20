0d235102a51270f6fa4e45def1c914769f1f1008
mrtada <maitandat1507@gmail.com>
2018-10-20 20:27:01 +0700

    repare structure of feature Recipe

--- src/js/index.js
+++ src/js/index.js
@@ -55,6 +55,18 @@ elements.searchResPages.addEventListener('click', e => {
 /**
  * Recipe Controller
  */
-const r = new Recipe(46956);
-r.getRecipe();
-console.log(r);
+const controlRecipe = () => {
+    // Get ID from url
+    const id = window.location.hash.replace('#', '');
+    console.log(id);
+
+    if (id) {
+        // Prepare UI for changes
+
+        // Create new recipe object
+
+        // Get recipe
+    }
+}
+
+window.addEventListener('hashchange', controlRecipe);
