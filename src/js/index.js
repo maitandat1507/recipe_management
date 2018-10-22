fc6c42d7cbd6402a748faa0377fddbfae42d462e
mrtada <maitandat1507@gmail.com>
2018-10-23 00:35:43 +0700

    handle recipe model

--- src/js/index.js
+++ src/js/index.js
@@ -36,11 +36,18 @@ const controlSearch = async () => {
             searchView.renderResults(state.search.result);
         } catch (err) {
             alert('Songthing wrong with the search... :(');
+            clearLoader();
         }
     }
 }
 
 elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
+// TESTING
+window.addEventListener('load', e => {
     e.preventDefault();
     controlSearch();
 });
@@ -71,8 +78,10 @@ const controlRecipe = async () => {
         state.recipe = new Recipe(id);
 
         try {
-            // Get recipe data
+            // Get recipe data and parse ingredients
             await state.recipe.getRecipe();
+            console.log(state.recipe.ingredients);
+            state.recipe.parseIngredients();
     
             // Calculate servings and time
             state.recipe.calcTime();
