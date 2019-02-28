57fe0dcba2576b924ec3d3f0f5749ed7946a8368
maitandat1507 <maitandat1507@gmail.com>
2019-02-28 15:00:26 +0700

    finish Shopping list controller

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,7 @@ import Recipe from './models/Recipe';
 import List from './models/List';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/recipeView';
+import * as listView from './views/listView';
 import {elements, renderLoader, clearLoader} from './views/base';
 
 /** Global state of the app
@@ -12,6 +13,7 @@ import {elements, renderLoader, clearLoader} from './views/base';
  * - Liked recipes
  */
 const state = {};
+window.state = state;
 
 /**
  * Search Controller
@@ -116,12 +118,29 @@ const controlList = () => {
     // Create a new list If there in none yet
     if (!state.list) state.list = new List();
 
-    // Add each ingredient to the list
+    // Add each ingredient to the list and UI
     state.recipe.ingredients.forEach(el => {
-        state.list.addItem(el.count, el.unit, el.ingredients);
-        //
+        const item = state.list.addItem(el.count, el.unit, el.ingredient);
+        listView.renderItem(item);
     });
 }
+
+// Handle delete and update list item events
+elements.shopping.addEventListener('click', e => {
+    const id = e.target.closest('.shopping__item').dataset.itemid;
+
+    // Handle the delete button
+    if (e.target.matches('.shopping__delete, .shopping__delete *')) {
+        // Delete from state
+        state.list.deleteItem(id);
+
+        //Delete from UI
+        listView.deleteItem(id);
+    } else if (e.target.matches('.shopping__count-value')) {
+        const val = parseFloat(e.target.value, 10);
+        state.list.updateCount(id, val);
+    }
+});
 
 
 // Handling recipe button clicks
